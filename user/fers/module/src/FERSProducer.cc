8bcc07361e2e66f2401ea85712b4e1820a943d18
Rino Persiani <persiani@lns.infn.it>
2023-01-23 13:34:00 +0100

    Added code for fers configuration path and file.

--- user/fers/module/src/FERSProducer.cc
+++ user/fers/module/src/FERSProducer.cc
@@ -7,8 +7,6 @@
 
 
 #include "eudaq/Producer.hh"
-#include "FERS_Registers.h"
-#include "FERSlib.h"
 #include <iostream>
 #include <fstream>
 #include <ratio>
@@ -16,10 +14,24 @@
 #include <thread>
 #include <random>
 #include "stdlib.h"
+#include "JanusC.h"
+#include "Statistics.h"
+#include "FERS_Registers.h"
+#include "FERSutils.h"
+#include "paramparser.h"
+#include "FERSlib.h"
+#include "configure.h"
 #ifndef _WIN32
 #include <sys/file.h>
 #endif
 //----------DOC-MARK-----BEG*DEC-----DOC-MARK----------
+
+
+  Config_t WDcfg;		// struct with all parameters
+  RunVars_t RunVars;            // struct containing run time variables
+  int SockConsole = 0;
+  char ErrorMsg[250];
+
 class FERSProducer : public eudaq::Producer {
   public:
   FERSProducer(const std::string & name, const std::string & runcontrol);
@@ -32,16 +44,20 @@ class FERSProducer : public eudaq::Producer {
   void RunLoop() override;
   
   static const uint32_t m_id_factory = eudaq::cstr2hash("FERSProducer");
+
+  
 private:
   bool m_flag_ts;
   bool m_flag_tg;
   uint32_t m_plane_id;
   FILE* m_file_lock;
+  FILE* conf_file;
   std::chrono::milliseconds m_ms_busy;
   bool m_exit_of_run;
-
   std::string fers_ip_address;  // IP address of the board
   int handle;		 	// Board handle
+
+  float vmon, imon, vbias, imax;
 };
 //----------DOC-MARK-----END*DEC-----DOC-MARK----------
 //----------DOC-MARK-----BEG*CON-----DOC-MARK----------
@@ -52,7 +68,8 @@ namespace{
 //----------DOC-MARK-----END*REG-----DOC-MARK----------
 
 FERSProducer::FERSProducer(const std::string & name, const std::string & runcontrol)
-  :eudaq::Producer(name, runcontrol), m_file_lock(0), m_exit_of_run(false){  
+  :eudaq::Producer(name, runcontrol), m_file_lock(0), m_exit_of_run(false){
+  //conf_file = NULL;
 }
 
 //----------DOC-MARK-----BEG*INI-----DOC-MARK----------
@@ -72,14 +89,20 @@ void FERSProducer::DoInitialise(){
   char connection_path[40];
   strcpy(ip_address, fers_ip_address.c_str());
   sprintf(connection_path,"eth:%s",ip_address);
-  std::cout <<"----3333---- "<<connection_path<<std::endl;
-  int ret = FERS_OpenDevice(connection_path, &handle);
-  std::cout <<"-------- ret= "<<ret<<std::endl;
+  // std::cout <<"----3333---- "<<connection_path<<std::endl;
+  int ret  = FERS_OpenDevice(connection_path, &handle);
+  //FERS_ReadBoardInfo() // return=0 e riempie la struct FERS_BoardInfo
+  // std::cout <<"-------- ret= "<<ret<<std::endl;
   if(ret == 0)
+    {
     std::cout <<"Connected to: "<< connection_path<<std::endl;
+    ret = FERS_ReadBoardInfo(*handle, FERS_BoardInfo[0]); //FERS_BoardInfo[BoardIndex]);
+		if ((ret != 0) && (ret != FERSLIB_ERR_INVALID_BIC)) return ret;
+    }
   else
     EUDAQ_THROW("unable to connect to fers with ip address: "+ fers_ip_address);
-  std::cout <<" ------- RINO ----------   "<<fers_ip_address<<std::endl;  
+  
+  // std::cout <<" ------- RINO ----------   "<<fers_ip_address<<std::endl;  
 }
 
 
@@ -87,8 +110,45 @@ void FERSProducer::DoInitialise(){
 void FERSProducer::DoConfigure(){
   auto conf = GetConfiguration();
   conf->Print(std::cout);
-  std::string m_prova = conf->Get("PROVA", "pippo");
-  std::cout <<"######## RINO ###########  "<<m_prova<<std::endl;
+  int ret = -1000;
+  int conf_verbose = conf->Get("FERS_CONF_VERBOSE",0);
+  std::string fers_conf_dir = conf->Get("FERS_CONF_DIR", ".");
+  std::string fers_conf_file = conf->Get("FERS_CONF_FILE", "configuration_0.txt");
+  std::string conf_filename = fers_conf_dir+fers_conf_file;
+
+
+
+  HV_Get_Vmon(handle, &vmon);
+  HV_Get_Imon(handle, &imon);
+  printf("ecco i valori di Vmon e Imon prima della configurazione: %f \t %f \n",vmon,imon);
+  
+  if(conf_verbose>=1)
+    std::cout <<"Opening configuration file: "<<conf_filename<<std::endl;
+  conf_file = fopen(conf_filename.c_str(),"r");
+  if(conf_file == NULL)
+    EUDAQ_THROW("unable to open configuration file: " + (std::string)(conf_filename));
+  else {
+    ret = ParseConfigFile(conf_file, &WDcfg, 1);
+    if(ret == 0)
+      std::cout <<"Board properly configured."<<std::endl;
+    fclose(conf_file);
+  }
+
+  // if()
+  RunVars.StaircaseCfg[SCPARAM_MIN] = 200;
+  RunVars.StaircaseCfg[SCPARAM_MAX] = 300;
+  RunVars.StaircaseCfg[SCPARAM_STEP] = 1;
+  RunVars.StaircaseCfg[SCPARAM_DWELL] = 500;
+
+  ConfigureFERS(handle,0);
+  
+  HV_Get_Vmon(handle, &vmon);
+  HV_Get_Imon(handle, &imon);
+  printf("ecco i valori di Vmon e Imon dopo la configurazione: %f \t %f \n",vmon,imon);
+  HV_Get_Vbias(handle, &vbias);
+  HV_Get_Imax(handle, &imax);
+  printf("ecco vbias e imax: %f \t %f \n",vbias,imax);
+  
   m_plane_id = conf->Get("EX0_PLANE_ID", 0);
   m_ms_busy = std::chrono::milliseconds(conf->Get("EX0_DURATION_BUSY_MS", 1000));
   m_flag_ts = conf->Get("EX0_ENABLE_TIMESTAMP", 0);
@@ -104,11 +164,30 @@ void FERSProducer::DoConfigure(){
 void FERSProducer::DoStartRun(){
   m_exit_of_run = false;
   // here the hardware is told to startup
+
+  HV_Get_Vmon(handle, &vmon);
+  HV_Get_Imon(handle, &imon);
+  printf("ecco i valori di Vmon e Imon allo start run: %f \t %f \n",vmon,imon);
+  printf("----- ORA ACCENDO L'HV ----- \n");
+  Sleep(100);
+  HV_Set_OnOff(handle, 1);
+  HV_Get_Vmon(handle, &vmon);
+  HV_Get_Imon(handle, &imon);
+  printf("ecco i valori di Vmon e Imon allo start run: %f \t %f \n",vmon,imon);
 }
 
 //----------DOC-MARK-----BEG*STOP-----DOC-MARK----------
 void FERSProducer::DoStopRun(){
   m_exit_of_run = true;
+  HV_Get_Vmon(handle, &vmon);
+  HV_Get_Imon(handle, &imon);
+  printf("ecco i valori di Vmon e Imon allo stop run: %f \t %f \n",vmon,imon);
+  printf("----- ORA SPENGO L'HV ----- \n");
+  Sleep(100);
+  HV_Set_OnOff(handle, 0);
+  HV_Get_Vmon(handle, &vmon);
+  HV_Get_Imon(handle, &imon);
+  printf("ecco i valori di Vmon e Imon allo stop run: %f \t %f \n",vmon,imon);
 }
 
 //----------DOC-MARK-----BEG*RST-----DOC-MARK----------
@@ -132,10 +211,26 @@ void FERSProducer::DoTerminate(){
     fclose(m_file_lock);
     m_file_lock = 0;
   }
+  
+  HV_Get_Vmon(handle, &vmon);
+  HV_Get_Imon(handle, &imon);
+  printf("ecco i valori di Vmon e Imon al terminate run: %f \t %f \n",vmon,imon);
+  printf("----- ORA SPENGO NUOVAMENTE L'HV ----- \n");
+  Sleep(100);
+  HV_Set_OnOff(handle, 0);
+  HV_Get_Vmon(handle, &vmon);
+  HV_Get_Imon(handle, &imon);
+  printf("ecco i valori di Vmon e Imon al terminate run: %f \t %f \n",vmon,imon);
 }
 
 //----------DOC-MARK-----BEG*LOOP-----DOC-MARK----------
 void FERSProducer::RunLoop(){
+
+  
+  
+  //ScanThreshold(handle);
+  //ConfigureFERS(handle, CFG_HARD);
+  
   auto tp_start_run = std::chrono::steady_clock::now();
   uint32_t trigger_n = 0;
   uint8_t x_pixel = 16;
@@ -145,6 +240,11 @@ void FERSProducer::RunLoop(){
   std::uniform_int_distribution<uint32_t> position(0, x_pixel*y_pixel-1);
   std::uniform_int_distribution<uint32_t> signal(0, 255);
   while(!m_exit_of_run){
+    sleep(0.001);
+    HV_Get_Vmon(handle, &vmon);
+    HV_Get_Imon(handle, &imon);
+    printf("Vmon e Imon nel run: %f \t %f \n",vmon,imon);
+    
     auto ev = eudaq::Event::MakeUnique("FERSRaw");
     ev->SetTag("Plane ID", std::to_string(m_plane_id));
     auto tp_trigger = std::chrono::steady_clock::now();
